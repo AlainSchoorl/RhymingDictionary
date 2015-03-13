610c404bad2ad0628a1a937a07573d699ce010cd
DeadPlatypus <pyromaniak5@gmail.com>
2015-03-13 18:11:25 +0100

    GUI, WordList into HashSet

--- src/RhymingDictionary/Phoneme.java
+++ src/RhymingDictionary/Phoneme.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rhyming.dictionary;
+package RhymingDictionary;
 import java.lang.Math;
 import java.util.ArrayList;
 /**
