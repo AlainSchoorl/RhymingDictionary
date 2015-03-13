15bc7350ee8771636290487c41a52b07123cbc09
DeadPlatypus <pyromaniak5@gmail.com>
2015-03-13 18:10:03 +0100

    Converted WordList into a HashSet HashSets are lists with only one of each element

---
+++ src/RhymingDictionary/WordList.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RhymingDictionary;
+import java.util.HashSet;
+/**
+ *
+ * @author Alain
+ */
+public class WordList extends HashSet<Word> {
+
+    public Word getWord(String s)
+    {
+        for(Word w : this) //For every word in the list
+        {
+            if(w.isWord(s)) //If the strings match, return that word
+            {
+                return w;
+            }
+        }
+        return null; // Need to find something to return if the word isn't in our dictionary.
+    }
+    
+    public WordList getPerfectRhymes(Word w, int i)
+    {
+        WordList rhymes = new WordList(); //Creates empty wordlist (I hope)
+        for(Word r : this) //For every word in the list
+        {
+            if(r.isPerfectRhyme(w, i)) //If it is a perfect rhyme, add it to the rhyme list
+            {
+                rhymes.add(r);
+            }
+        }
+        return rhymes; //Return the rhyme list
+    }
+    public WordList getFamilyRhymes(Word w, int i)
+    {
+        WordList rhymes = new WordList();
+        for(Word r : this)
+        {
+            if(r.isFamilyRhyme(w, i))
+            {
+                rhymes.add(r);
+            }
+        }
+        return rhymes;
+    }
+    public WordList getAdditiveRhymes(Word w, int i)
+    {
+        WordList rhymes = new WordList();
+        for(Word r : this)
+        {
+            if(r.isAdditiveRhyme(w, i))
+            {
+                rhymes.add(r);
+            }
+        }
+        return rhymes;
+    }
+    public WordList getSubtractiveRhymes(Word w, int i)
+    {
+        WordList rhymes = new WordList();
+        for(Word r : this)
+        {
+            if(r.isSubtractiveRhyme(w, i))
+            {
+                rhymes.add(r);
+            }
+        }
+        return rhymes;
+    }
+    public WordList getAssonanceRhymes(Word w, int i)
+    {
+        WordList rhymes = new WordList();
+        for(Word r : this)
+        {
+            if(r.isAssonanceRhyme(w, i))
+            {
+                rhymes.add(r);
+            }
+        }
+        return rhymes;
+    }
+    public WordList getConsonanceRhymes(Word w, int i)
+    {
+        WordList rhymes = new WordList();
+        for(Word r : this)
+        {
+            if(r.isConsonanceRhyme(w, i))
+            {
+                rhymes.add(r);
+            }
+        }
+        return rhymes;
+    }
+    public WordList getHalfRhymes(Word w, int i)
+    {
+        WordList rhymes = new WordList();
+        for(Word r : this)
+        {
+            if(r.isHalfRhyme(w, i))
+            {
+                rhymes.add(r);
+            }
+        }
+        return rhymes;
+    }
+}
